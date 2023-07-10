61fe8c2def23f8229f7820f91695e23709de32a9
Raulgaray <raul.garay@tecsup.edu.pe>
2023-07-10 05:01:14 -0500

    first commit

---
+++ src/components/layout/private/SideBar.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import avatar from '../../../assets/img/user.png'
+import useAuth from "../../../hooks/useAuth";
+import { Global } from "../../../helpers/Global";
+
+export const SideBar = () => {
+
+
+  const { auth, counters } = useAuth();
+
+  console.log(auth, counters);
+
+
+  return (
+    <aside className="layout__aside">
+      <header className="aside__header">
+        <h1 className="aside__title">Hola, {auth.name}</h1>
+      </header>
+
+      <div className="aside__container">
+        <div className="aside__profile-info">
+          <div className="profile-info__general-info">
+            <div className="general-info__container-avatar">
+              {auth.image != "default.png" && <img
+                src={Global.url + "user/avatar/" + auth.image}
+                className="container-avatar__img"
+                alt="Foto de perfil"
+              />}
+              {auth.image == "default.png" && <img
+                src={avatar}
+                className="container-avatar__img"
+                alt="Foto de perfil"
+              />}
+            </div>
+
+            <div className="general-info__container-names">
+              <a href="#" className="container-names__name">
+                {auth.name} {auth.surname}
+              </a>
+              <p className="container-names__nickname">{auth.nick}</p>
+            </div>
+          </div>
+
+          <div className="profile-info__stats">
+            <div className="stats__following">
+              <a href="#" className="following__link">
+                <span className="following__title">Siguiendo</span>
+                <span className="following__number">{counters.following}</span>
+              </a>
+            </div>
+            <div className="stats__following">
+              <a href="#" className="following__link">
+                <span className="following__title">Seguidores</span>
+                <span className="following__number">{counters.followed}</span>
+              </a>
+            </div>
+
+            <div className="stats__following">
+              <a href="#" className="following__link">
+                <span className="following__title">Publicaciones</span>
+                <span className="following__number">{counters.publications}</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="aside__container-form">
+          <form className="container-form__form-post">
+            <div className="form-post__inputs">
+              <label htmlFor="post" className="form-post__label">
+                ¿Que estas pesando hoy?
+              </label>
+              <textarea name="post" className="form-post__textarea"></textarea>
+            </div>
+
+            <div className="form-post__inputs">
+              <label htmlFor="image" className="form-post__label">
+                Sube tu foto
+              </label>
+              <input type="file" name="image" className="form-post__image" />
+            </div>
+
+            <input
+              type="submit"
+              value="Enviar"
+              className="form-post__btn-submit"
+              disabled
+            />
+          </form>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+
+
+
---
+++ src/components/user/Config.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import useAuth from '../../hooks/useAuth';
+import { Global } from '../../helpers/Global';
+import { SerializeForm } from '../../helpers/SerializeForm';
+
+export const Config = () => {
+
+    const { auth, setAuth } = useAuth();
+
+    const [saved, setSaved] = useState("not_saved");
+
+    const updateUser = async(e) => {
+        e.preventDefault();
+
+        // Recoger datos del formulario
+        let newDataUser = SerializeForm(e.target);
+
+        // Borrar propiedad innecesaria
+        delete newDataUser.file0;
+
+        // Actualizar usuario en la base de datos
+        const request = await fetch(Global.url + "user/update",{
+            method: "PUT",
+            body: JSON.stringify(newDataUser),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            }
+        })
+        const data = await request.json();
+
+        if(data.status == "success"){
+            delete data.user.password;
+            setAuth(data.user);
+            setSaved("saved");
+
+            console.log(auth);
+        }else{
+            setSaved("error");
+        }
+    }
+
+
+    return (
+        <>
+            <header className="content__header content__header--public">
+                <h1 className="content__title">Registro</h1>
+            </header>
+
+            <div className="content__posts">
+
+                {saved == "saved" ?
+                    <strong className="alert alert-success"> Usuario actualizado Correctamente !! </strong>
+                    : ''}
+
+                {saved == "error" ?
+                    <strong className="alert alert-danger"> Usuario no se ha actualizado !!</strong>
+                    : ''}
+
+                <form className="config-form" onSubmit={updateUser}>
+
+                    <div className="form-group">
+                        <label htmlFor="name">Nombre</label>
+                        <input type="text" name="name" defaultValue={auth.name} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="surname">Apellidos</label>
+                        <input type="text" name="surname" defaultValue={auth.surname} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="nick">Nick</label>
+                        <input type="text" name="nick" defaultValue={auth.nick} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="bio">Biografia</label>
+                        <textarea name="bio" defaultValue={auth.bio} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email">Correo Electronico</label>
+                        <input type="email" name="email" defaultValue={auth.email} />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" name="password" />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="file0">Avatar</label>
+                        <div className='general-info__container-avatar'>
+                            {auth.image != "default.png" && <img
+                                src={Global.url + "user/avatar/" + auth.image}
+                                className="container-avatar__img"
+                                alt="Foto de perfil"
+                            />}
+                            {auth.image == "default.png" && <img
+                                src={avatar}
+                                className="container-avatar__img"
+                                alt="Foto de perfil"
+                            />}
+                        </div>
+                        <br />
+                        <input type="file" name="file0" id="file" />
+                    </div>
+                    <br />
+
+                    <input type="submit" value="Actualizar" className="btn btn-success" />
+                </form>
+                <br />
+
+            </div>
+        </>
+    )
+}
