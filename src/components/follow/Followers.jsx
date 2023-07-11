c45cf87e522cbce52e36b775cad204b3b7a676ea
samuel-rome <samuel2004romero@gmail.com>
2023-07-11 05:41:42 -0500

    Commit Form

--- src/components/follow/Followers.jsx
+++ src/components/follow/Followers.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Global } from "../../helpers/Global";
 import { UserList } from '../user/UserList';
 import { useParams } from 'react-router-dom';
+import { GetProfile } from '../../helpers/getProfile';
 
 
 export const Followers = () => {
@@ -11,11 +12,14 @@ export const Followers = () => {
   const [more, setMore] = useState(true);
   const [following, setFollowing] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [userProfile, setUserProfile] = useState({});
+
 
     const params = useParams();
 
   useEffect(() => {
     getUsers(1);
+    GetProfile(params.userId, setUserProfile);
   }, []);
 
   const getUsers = async (nextPage = 1) => {
@@ -71,7 +75,7 @@ export const Followers = () => {
   return (
     <>
       <header className="content__header">
-        <h1 className="content__title">Seguidores "NOMBRE USUARIO"</h1>
+        <h1 className="content__title">Seguidores de {userProfile.name} {userProfile.surname}</h1>
       </header>
 
       <UserList users={users}
--- src/components/layout/private/SideBar.jsx
+++ src/components/layout/private/SideBar.jsx
@@ -1,14 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import avatar from '../../../assets/img/user.png'
 import useAuth from "../../../hooks/useAuth";
 import { Global } from "../../../helpers/Global";
 import { Link } from "react-router-dom";
+import { useForm } from "../../../hooks/useForm";
 
 export const SideBar = () => {
 
 
   const { auth, counters } = useAuth();
+  const { form, changed } = useForm({});
+  const [stored, setStored] = useState("not_stored");
 
+
+  const token = localStorage.getItem("token");
+
+
+  const savePublication = async (e) => {
+    e.preventDefault();
+
+    // Recoger datos del formulario
+    let newPublication = form;
+    newPublication.user = auth._id;
+
+    // Hacer request para guardar en bd
+    const request = await fetch(Global.url + "publication/save", {
+      method: "POST",
+      body: JSON.stringify(newPublication),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
+      }
+    });
+
+    const data = await request.json();
+    // Mostrar mensaje de exito o error
+    if (data.status = "success") {
+      setStored("stored")
+    } else {
+      setStored("error")
+    }
+
+
+    // Subir imagen
+
+    const fileInput = document.querySelector("#file");
+
+    if(data.status == "success" && fileInput.files[0]){
+
+        const formData = new FormData();
+        formData.append("file0", fileInput.files[0]);
+
+        const uploadRequest = await fetch(Global.url + "publication/upload/" + data.publicationStored._id,{
+          method: "POST",
+          body: formData,
+          headers: {
+            "Authorization": token
+          }
+        });
+
+        const uploadData = await uploadRequest.json();
+
+        if(uploadData.status == "success"){
+          setStored("stored");
+        }else{
+          setStored("error");
+        }
+
+        if (data.status = "success" && uploadData.status == "success"){
+          const myForm = document.querySelector("#publication-form");
+          myForm.reset();
+        }
+    }
+  }
 
 
   return (
@@ -43,7 +107,7 @@ export const SideBar = () => {
             </div>
 
             <div className="stats__following">
-            <Link to={"seguidores/" + auth._id} className="following__link">
+              <Link to={"seguidores/" + auth._id} className="following__link">
                 <span className="following__title">Seguidores</span>
                 <span className="following__number">{counters.followed}</span>
               </Link>
@@ -55,31 +119,41 @@ export const SideBar = () => {
                 <span className="following__number">{counters.publications}</span>
               </a>
             </div>
-            
+
           </div>
         </div>
 
         <div className="aside__container-form">
-          <form className="container-form__form-post">
+
+          {stored == "stored" ?
+            <strong className="alert alert-success"> Publicada Correctamente !! </strong>
+            : ''}
+
+          {stored == "error" ?
+            <strong className="alert alert-danger"> Publicacion ERROR !!</strong>
+            : ''}
+
+          <form id="publication-form" className="container-form__form-post" onSubmit={savePublication}>
+
             <div className="form-post__inputs">
-              <label htmlFor="post" className="form-post__label">
+              <label htmlFor="text" className="form-post__label">
                 ¿Que estas pesando hoy?
               </label>
-              <textarea name="post" className="form-post__textarea"></textarea>
+              <textarea name="text" className="form-post__textarea" onChange={changed} />
             </div>
 
             <div className="form-post__inputs">
-              <label htmlFor="image" className="form-post__label">
+              <label htmlFor="file" className="form-post__label">
                 Sube tu foto
               </label>
-              <input type="file" name="image" className="form-post__image" />
+              <input type="file" name="file0" id="file" className="form-post__image" />
             </div>
 
             <input
               type="submit"
               value="Enviar"
               className="form-post__btn-submit"
-              disabled
+              
             />
           </form>
         </div>
