8f5279af35d55d38b9ac92b13e636144e1d3a001
samuel-rome <samuel2004romero@gmail.com>
2023-07-11 10:11:04 -0500

    Commit Estable

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 import { GetProfile } from '../../helpers/GetProfile'
 import { Global } from '../../helpers/Global';
 import useAuth from '../../hooks/useAuth';
+import { PublicationList } from '../publication/PublicationList';
 
 
 
@@ -137,27 +138,9 @@ export const Profile = () => {
     }
   }
 
-  const nextPage = () => {
-    let next = page + 1;
-    setPage(next);
-    getPublications(next);
-  }
+  
 
-  const deletePublication = async(publicationId) => {
-    const request = await fetch(Global.url + "publication/remove/" + publicationId, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": localStorage.getItem("token")
-      }
-    });
-
-    const data = await request.json();
-    setPage(1);
-    setMore(true);
-    getPublications(1, true);
-    
-  }
+  
 
 
 
@@ -221,105 +204,16 @@ export const Profile = () => {
 
 
 
-      <div className="content__posts">
+      <PublicationList 
+              publications={publications}
+              getPublications={getPublications}
+              page={page}
+              setPage={setPage}
+              more={more}
+              setMore={setMore}
+      />
 
-        {publications.map(publication => {
-
-          return (
-
-            <article className="posts__post" key={publication._id}>
-
-              <div className="post__container">
-
-                <div className="post__image-user">
-
-                  <Link to={"/social/perfil/" + publication.user._id} className="post__image-link">
-                    {publication.user.image != "default.png" && <img src={Global.url + "user/avatar/" + publication.user.image} className="post__user-image" alt="Foto de perfil" />}
-                    {publication.user.image == "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-                  </Link>
-                </div>
-
-                <div className="post__body">
-                  <div className="post__user-info">
-                    <a href="#" className="user-info__name">
-                      {publication.user.name + "" + publication.user.surname}
-                    </a>
-                    <span className="user-info__divider"> | </span>
-                    <a href="#" className="user-info__create-date">
-                      {publication.created_at}
-                    </a>
-                  </div>
-
-                  <h4 className="post__content">{publication.text}</h4>
-
-                  {/* PARA SUBIR IMAGENES A LA PUBLICACION FALTA  */}
-                 {/* {publication.file && <img src={Global.url + "publication/media/" + publication.file} />} */}
-
-                </div>
-              </div>
-
-
-              {auth._id == publication.user._id &&
-                <div className="post__buttons">
-                  <a href="#" className="post__button">
-                    <i className="fa-solid fa-trash-can"></i>
-                  </a>
-                </div>
-              }
-            </article>);
-
-        })};
-
-        {/* {publications.map((publication, index) => (
-          <article className="posts__post" key={`${publication._id}-${index}`}>
-
-            <div className="post__container">
-              <div className="post__image-user">
-                <Link to={"/social/perfil/" + publication.user._id} className="post__image-link">
-                  {publication.user.image !== "default.png" && <img src={Global.url + "user/avatar/" + publication.user.image} className="post__user-image" alt="Foto de perfil" />}
-                  {publication.user.image === "default.png" && <img src={avatar} className="post__user-image" alt="Foto de perfil" />}
-                </Link>
-              </div>
-
-              <div className="post__body">
-                <div className="post__user-info">
-                  <a href="#" className="user-info__name">
-                    {publication.user.name + " " + publication.user.surname}
-                  </a>
-                  <span className="user-info__divider"> | </span>
-                  <a href="#" className="user-info__create-date">
-                    {publication.created_at}
-                  </a>
-                </div>
-
-                <h4 className="post__content">{publication.text}</h4>
-
-                {publication.file && <img src={Global.url + "publication/media/" + publication.file} />}
-              </div>
-            </div>
-
-            {auth._id === publication.user._id &&
-              <div className="post__buttons">
-                <button onClick={() => deletePublication(publication._id)} className="post__button">
-                  <i className="fa-solid fa-trash-can"></i>
-                </button>
-              </div>
-            }
-
-          </article>
-        ))} */}
-
-
-
-      </div>
-
-      {more &&
-        <div className='content__container-btn'>
-          <button className='content__btn-more-post' onClick={nextPage}>
-            ver mas publicaciones
-          </button>
-        </div>
-      }
+      
       <br />
     </>
   )
